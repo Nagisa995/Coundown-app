616414cd3642ba49a58c8b8d446573ce7cf23549
Nagisa <miku195@mail.ru>
2022-04-09 02:10:22 +0700

    eslint Google fix

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
 import {
   DEFAULT_UI_ELEMENTS,
-  COUNTDOWN_COMPLETE
-} from './const'
+  COUNTDOWN_COMPLETE,
+} from './const';
 
 import {
-  countdownTimeOnUI
-} from './view'
+  countdownTimeOnUI,
+} from './view';
 
 import {
   addSetDateInStorage,
   getSetDateFromStorage,
-  clearStorage
-} from './storage'
+  clearStorage,
+} from './storage';
 
 import {
   intervalToDuration,
   isAfter,
-  formatDuration
-} from 'date-fns'
+  formatDuration,
+} from 'date-fns';
 
-DEFAULT_UI_ELEMENTS.UI_FORM.addEventListener('submit', countdownDate)
+DEFAULT_UI_ELEMENTS.UI_FORM.addEventListener('submit', countdownDate);
 
-function countdownDate(){    
-  event.preventDefault()
+function countdownDate() {
+  event.preventDefault();
 
-  const setDate = new Date(DEFAULT_UI_ELEMENTS.UI_SET_DATE.value)
+  const setDate = new Date(DEFAULT_UI_ELEMENTS.UI_SET_DATE.value);
 
-  addSetDateInStorage(setDate)
-  countdown()
+  addSetDateInStorage(setDate);
+  countdown();
 }
 
-function countdown (){
-  const setDate = getSetDateFromStorage()
-  const countdownIsComplete = isAfter(Date.now(), setDate)
+function countdown() {
+  const setDate = getSetDateFromStorage();
+  const countdownIsComplete = isAfter(Date.now(), setDate);
 
   if (countdownIsComplete) {
-    clearStorage()
-    return countdownTimeOnUI(COUNTDOWN_COMPLETE)
+    clearStorage();
+    return countdownTimeOnUI(COUNTDOWN_COMPLETE);
   }
 
-  const countdownTime = intervalToDuration({start: Date.now(), end:setDate})
+  const countdownTime = intervalToDuration({start: Date.now(), end: setDate});
 
-  countdownTimeOnUI(formatDuration(countdownTime))
+  countdownTimeOnUI(formatDuration(countdownTime));
 
-  setTimeout(countdown, 1e3)
+  setTimeout(countdown, 1e3);
 }
--- js/storage.js
+++ js/storage.js
@@ -1,11 +1,11 @@
-export function addSetDateInStorage(setDate){
-  localStorage.setItem('Set Date', setDate)
+export function addSetDateInStorage(setDate) {
+  localStorage.setItem('Set Date', setDate);
 }
 
-export function getSetDateFromStorage(){
-  return new Date(localStorage.getItem('Set Date'))
+export function getSetDateFromStorage() {
+  return new Date(localStorage.getItem('Set Date'));
 }
 
-export function clearStorage(){
-  localStorage.clear()
+export function clearStorage() {
+  localStorage.clear();
 }
